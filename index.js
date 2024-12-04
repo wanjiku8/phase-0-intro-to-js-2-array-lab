f6698033264d83994ba91b2292f1063cab98dcd3
Faith Wanjiku <wanjikuf141@gmail.com>
2024-12-04 19:41:31 +0300

    commit

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 
 beforeEach(function () {
-    cats.length = 0;
-    cats.push("Milo", "Otis", "Garfield");
-  });
-  
+  cats.length = 0;  // Clear the array
+  cats.push("Milo", "Otis", "Garfield");  // Add initial values
+
+  console.log(cats);  // Log the array to the console
+});
